b8c227b6a9cc97660ad05064b19528c25c3128d4
JParedesArtica <paredesjavier23@gmail.com>
2023-11-15 14:52:23 -0500

    iniciando proyecto con node

--- .learn/exercises/03-first-express-app/solution.hide.js
+++ .learn/exercises/03-first-express-app/solution.hide.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
+console.log("Server running on port", port)
 const server = app.listen(8080, function () {
   let port = server.address().port
-  console.log("Server running on port", port)
 })
