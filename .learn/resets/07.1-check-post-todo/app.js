749e2c621bd1bd98e5c779adaa3894609e8f7b66
JParedesArtica <paredesjavier23@gmail.com>
2023-11-15 19:17:49 -0500

    solicitud delete todos

---
+++ .learn/resets/07.1-check-post-todo/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const todos = [
+    { "label": "Drink some water", "done": false },
+    { "label": "Do my homework", "done": false }
+];
+
+
+const server =app.listen(8080, function(){
+    let port= server.address().port
+    console.log("Server running on port",port)
+})
+
+app.get('/',(req,res)=>{
+    res.status(200).json({'name':'Joe Doe'})
+})
+
+app.get('/todos', (req,res)=>{
+    res.status(200).json(todos)
+})
+
+app.post('/signup', (req,res)=>{
+    const user = req.body;
+    todos.push(user);
+    res.status(200).json(todos);
+})
--- app.js
+++ app.js
@@ -2,16 +2,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const todos = [
+let todos = [
     { "label": "Drink some water", "done": false },
     { "label": "Do my homework", "done": false }
 ];
 
 
-const server =app.listen(8080, function(){
-    let port= server.address().port
-    console.log("Server running on port",port)
-})
 
 app.get('/',(req,res)=>{
     res.status(200).json({'name':'Joe Doe'})
@@ -21,8 +17,20 @@ app.get('/todos', (req,res)=>{
     res.status(200).json(todos)
 })
 
-app.post('/signup', (req,res)=>{
-    const user = req.body;
-    todos.push(user);
+app.post('/todos', (req,res)=>{
+    const todo = req.body;
+    todos.push(todo);
     res.status(200).json(todos);
+})
+
+app.delete('/todos/:todoPosition',(req,res)=>{
+    const todoPosition= req.params.todoPosition;
+    console.log(typeof todoPosition)
+    todos = todos.filter((value,position)=> position != todoPosition)
+    res.status(200).json(todos)
+})
+
+const server =app.listen(8080, function(){
+    let port= server.address().port
+    console.log("Server running on port",port)
 })
